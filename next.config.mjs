3bc9043a1b9779f9340103200169aaef20c8b75d
brbucknell17 <brbucknell17@gmail.com>
2025-11-09 11:50:03 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
-// next.config.mjs
 const isProd = process.env.NODE_ENV === "production";
-const repo = "portfolio"; // <-- your repo name
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",              // generate static files in 'out'
-  images: { unoptimized: true }, // Next/Image works without the optimizer
-  basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}/` : "",
-  trailingSlash: true,           // helps with static hosting
+const repo = "portfolio"; // your repo name
+export default {
+  output: "export",
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repo}` : undefined,
+  assetPrefix: isProd ? `/${repo}/` : undefined,
 };
 
-export default nextConfig;
